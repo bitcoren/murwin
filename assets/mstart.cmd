d4b0efd2402db387c50ff72b9d589fa8738d947a
Bitcoren <bitcoren@gmail.com>
2024-02-18 08:17:19 +0800

    Update mstart.cmd

--- assets/mstart.cmd
+++ assets/mstart.cmd
@@ -1,2 +1,2 @@
 start %MURZILLA%\murzilla.exe
-powershell -Command "Start-Process cmd '/c hidec \"C:\\Program Files\\IPFS Desktop\\IPFS Desktop.exe\"'"
+start %USERPROFILE%\AppData\Local\Programs\"IPFS Desktop"\"IPFS Desktop.exe"
